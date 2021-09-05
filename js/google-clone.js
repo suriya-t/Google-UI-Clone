45d6b74ff566b10d12cc58e8c5817584b1cfc8e0
suriya-t <86153076+suriya-t@users.noreply.github.com>
2021-09-05 21:49:32 +0530

    Update google-clone.js

--- js/google-clone.js
+++ js/google-clone.js
@@ -1,16 +1,20 @@
 const searchInput = document.getElementsByClassName("google-search-input");
+const searchButton = document.getElementsByClassName("search-button");
 
-/* Adding an event for the Input field to search when enter key is pressed*/
-searchInput[0].addEventListener("keydown", function(event){
+// Google Search the search term
+function searchResults() {
+  var url = `https://www.google.com/search?q=` + searchInput[0].value;
+
+  window.open(url, "_self");
+}
+
+// Adding an event for the Input field to search when enter key is pressed
+searchInput[0].addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
     searchResults();
   }
 });
 
-function searchResults()
-{
-  let url = "http://www.google.com/search?q=" + searchInput[0].value;
-
-  window.open(url, "_self");
-}
+// Adding an click event for the 'Google Search' button
+searchButton[0].addEventListener("click", searchResults);
